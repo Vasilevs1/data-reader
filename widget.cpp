8878276a264d9dfe24685275685bffbed6f3d052
Учащийся 207 <207@cdutt.local>
2016-05-07 19:31:18 +0300

    kinda working

--- widget.cpp
+++ widget.cpp
@@ -1,14 +1,20 @@
 #include "widget.h"
-
+#include <QVBoxLayout>
 
 Widget::Widget(QWidget *parent) :
     QWidget(parent)
 {
+    QVBoxLayout* layout = new QVBoxLayout;
 
     socket = new QUdpSocket;
-    edit = new QLineEdit(this);
-    socket->bind(QHostAddress::LocalHost,12000);
+    edit = new QLineEdit;
+    button = new QPushButton;
+    layout->addWidget(button);
+    layout->addWidget(edit);
+    this->setLayout(layout);
+    socket->bind(QHostAddress::Any,12000);
     connect(socket,SIGNAL(readyRead()),this,SLOT(displayData()));
+    connect(button,SIGNAL(clicked(bool)),this,SLOT(sendStuff()));
 }
 
 Widget::~Widget()
@@ -30,6 +36,13 @@ void Widget::displayData()
         int x,y;
         stream>>x>>y;
         edit->setText(QString::number(x)+QString::number(y));
-    }
 
+    }
+}
+
+void Widget::sendStuff()
+{
+    QByteArray d("PLEASE WORK");
+    int sent = socket->writeDatagram(d,QHostAddress::LocalHost,13000);
+    qDebug() << sent;
 }
--- widget.h
+++ widget.h
@@ -4,17 +4,20 @@
 #include <QWidget>
 #include <QUdpSocket>
 #include <QLineEdit>
+#include <QPushButton>
 class Widget : public QWidget
 {
     Q_OBJECT
     QUdpSocket* socket;
     QLineEdit* edit;
+    QPushButton* button;
 public:
     explicit Widget(QWidget *parent = 0);
     ~Widget();
 
 private slots:
     void displayData();
+    void sendStuff();
 
 };
 
